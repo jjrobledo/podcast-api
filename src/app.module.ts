8bf8a593030a2ef3585cfc48418f4e92cff2b9e9
Jesse Robledo <robledo.josej@gmail.com>
2023-09-09 17:35:44 -0600

    updated imports for user and podcast modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { PodcastModule } from './podcast/podcast.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UserModule, PodcastModule],
 })
 export class AppModule {}
