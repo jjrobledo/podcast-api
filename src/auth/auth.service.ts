7ba97d61f0bdda7f15ace23df3433a076cdc58b4
Jesse Robledo <robledo.josej@gmail.com>
2023-09-10 16:16:37 -0600

    added user registration feature with password hashing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import * as argon from 'argon2';
+import { AuthDto } from './dto';
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  login() {}
+
+  async register(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    const user = await this.prisma.user.create({
+      data: {
+        username: dto.username,
+        email: dto.email,
+        hash,
+      },
+    });
+
+    delete user.hash;
+
+    return user;
+  }
+}
