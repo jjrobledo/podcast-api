9ee0ece7ebd3cb6694545a93edcf5e32a0baeb43
Jesse Robledo <robledo.josej@gmail.com>
2023-09-09 23:05:23 -0600

    added prisma module and prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PodcastModule } from './podcast/podcast.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, PodcastModule],
+  imports: [AuthModule, UserModule, PodcastModule, PrismaModule],
 })
 export class AppModule {}
