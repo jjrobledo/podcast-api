5048ba14fe19c9b13c29e01c957aec7cc53b73c1
Jesse Robledo <robledo.josej@gmail.com>
2023-09-10 16:17:29 -0600

    created auth controller for user crud

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
-@Controller({})
-export class AuthController {}
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('register')
+  register(@Body() dto: AuthDto) {
+    return this.authService.register(dto);
+  }
+  @Post('login')
+  login() {
+    this.authService.login();
+  }
+}
