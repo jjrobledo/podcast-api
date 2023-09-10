693b575ed51245bb896b9b16d49a00f4f50b6b84
Jesse Robledo <robledo.josej@gmail.com>
2023-09-10 16:27:50 -0600

    added error handling - server no loger crashes when duplicate user tries to sign up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import * as argon from 'argon2';
 import { AuthDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
@@ -11,16 +12,26 @@ export class AuthService {
   async register(dto: AuthDto) {
     const hash = await argon.hash(dto.password);
 
-    const user = await this.prisma.user.create({
-      data: {
-        username: dto.username,
-        email: dto.email,
-        hash,
-      },
-    });
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          username: dto.username,
+          email: dto.email,
+          hash,
+        },
+      });
 
-    delete user.hash;
+      delete user.hash;
 
-    return user;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Invalid credentials');
+        }
+      } else {
+        throw error;
+      }
+    }
   }
 }
