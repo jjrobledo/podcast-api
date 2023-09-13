70294c2c642d3b63760c95229e9e9c1f6863477a
Jesse Robledo <robledo.josej@gmail.com>
2023-09-13 17:21:23 -0600

    updated to use test db

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,3 +1,23 @@
+import { addWaitHandler } from 'pactum/src/exports/handler';
+import { Test } from '@nestjs/testing';
+import { AppModule } from '../src/app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+
 describe('app-e2e', () => {
+  let app: INestApplication;
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+    await app.init();
+  });
   it.todo('should pass');
+  it.todo('should pass');
+
+  afterAll(() => {
+    app.close();
+  });
 });
