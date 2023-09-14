3c8dd0c60c8e2fb7c4dd1f5a14aef82752bc9dc3
Jesse Robledo <robledo.josej@gmail.com>
2023-09-13 21:13:16 -0600

    described additional test for e2e testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,9 +2,12 @@ import { addWaitHandler } from 'pactum/src/exports/handler';
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('app-e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
+
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -13,11 +16,33 @@ describe('app-e2e', () => {
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
+
+    prisma = app.get(PrismaService);
+
+    await prisma.cleanDb();
   });
-  it.todo('should pass');
-  it.todo('should pass');
 
   afterAll(() => {
     app.close();
+  });
+
+  describe('Auth', () => {
+    describe('Register', () => {
+      it.todo('should register');
+    });
+    describe('Login', () => {});
+  });
+
+  describe('User', () => {
+    describe('Get user', () => {});
+    describe('Edit user', () => {});
+    describe('Delete user', () => {});
+  });
+
+  describe('Bookmarks', () => {
+    describe('Register Podcast', () => {});
+    describe('Get podcast by id', () => {});
+    describe('Edit podcast by id', () => {});
+    describe('Delete podcast by id', () => {});
   });
 });
