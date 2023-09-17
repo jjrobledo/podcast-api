620cd2b087699095969d4a5078ef4537cc3658f7
Jesse Robledo <robledo.josej@gmail.com>
2023-09-16 22:41:05 -0600

    created service and controller for podcasts

--- src/podcast/podcast.module.ts
+++ src/podcast/podcast.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ControllerService } from './controller/controller.service';
+import { PodcastService } from './podcast.service';
 
-@Module({})
+@Module({
+  providers: [ControllerService, PodcastService]
+})
 export class PodcastModule {}
