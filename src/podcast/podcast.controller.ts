888c53ebac89c8f8b56bb3c97c46e96a6bc05fac
Jesse Robledo <robledo.josej@gmail.com>
2023-09-17 14:18:57 -0600

    moved podcast dtos to correct directory

--- src/podcast/podcast.controller.ts
+++ src/podcast/podcast.controller.ts
@@ -7,17 +7,19 @@ import {
   UseGuards,
   Param,
   ParseIntPipe,
+  Body,
 } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard';
 import { PodcastService } from './podcast.service';
 import { GetUser } from '../auth/decorator';
+import { CreatePodcastDto } from '../user/dto/dto';
 
 @UseGuards(JwtGuard)
 @Controller('podcasts')
 export class BookmarkController {
   constructor(private podcastService: PodcastService) {}
   @Post()
-  createPodcast(@GetUser() userId: number) {}
+  createPodcast(@GetUser() userId: number, @Body() dto: CreatePodcastDto) {}
 
   @Get()
   getPodcast(@GetUser() userId: number) {}
